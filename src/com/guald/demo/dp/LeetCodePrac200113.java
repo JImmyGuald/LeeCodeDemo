327cdebfc2c583d18b9b8f4740c4f53739ff72dd
jayden.gan <jayden.gan@welab-inc.com>
2020-01-14 10:31:57 +0800

    力扣

--- src/com/guald/demo/dp/LeetCodePrac200113.java
+++ src/com/guald/demo/dp/LeetCodePrac200113.java
@@ -36,5 +36,28 @@ package com.guald.demo.dp;
  * @create 2020/1/13 14:13
  */
 public class LeetCodePrac200113 {
+    //编辑差异算法 dp[i][j] 表示word1前i个字符到word2前j个字符的编辑差距D
+    //则可以得到当word1[i] == word2[j] 则 dp[i][j] = dp[i-1][j-1]
+    //当word1[i] != word2[j] 则 dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+    //其中dp[i-1][j-1] dp[i-1][j] dp[i][j-1] 分别对应替换、删除、插入操作
+    //边界情况dp[0][j] = j, dp[i][0] = i
+    public static int execute(String word1,String word2){
+        int[][] dp = new int[word1.length()+1][word2.length()+1];
+        for(int i = 0;i <= word1.length();i++){
+            for(int j = 0;j <= word2.length();j++) {
+                if(j ==0) {dp[i][j] = i;continue;}
+                if(i == 0) {dp[i][j] = j;continue;}
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j-1], dp[i-1][j]), dp[i][j-1]) + 1;
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
 
+    public static void main(String[] args) {
+        System.out.println(execute("horse","ros"));
+    }
 }
