997f0c51720f1f5a60e77be1fc7fdb56bac7dcb3
jayden <jayden.gan@welab-inc.com>
2020-02-10 18:19:03 +0800

    leetcode

--- src/com/guald/demo/bs/LC200130.java
+++ src/com/guald/demo/bs/LC200130.java
@@ -26,4 +26,51 @@ package com.guald.demo.bs;
  */
 public class LC200130 {
 
+    //二分法
+    public double findMedianSortednums1rrays(int[] nums1, int[] nums2) {
+            if(nums1.length>nums2.length){
+                int[] T = nums1;
+                nums1 = nums2;
+                nums2 = T;
+            }
+            int m = nums1.length;
+            int n = nums2.length;
+            int iMin = 0;
+            int iMax = m;
+            while (iMin <= iMax){
+                int i = (iMin+iMax)/2;
+                int j = (m+n+1)/2 - i;
+                if(i<iMax && nums2[j-1]>nums1[i]){ // i is small
+                    iMin = i+1;
+                } else if(i>iMin && nums1[i-1]>nums2[j]){ // i is large
+                    iMax = i-1;
+                } else { // i is perfect
+                    int maxLeft = 0;
+                    if (i == 0) { maxLeft = nums2[j-1]; }
+                    else if (j == 0) { maxLeft = nums1[i-1]; }
+                    else { maxLeft = Math.max(nums1[i-1], nums2[j-1]); }
+                    if ( (m + n) % 2 == 1 ) { return maxLeft; }
+
+                    int minRight = 0;
+                    if (i == m) { minRight = nums2[j]; }
+                    else if (j == n) { minRight = nums1[i]; }
+                    else { minRight = Math.min(nums2[j], nums1[i]); }
+
+                    return (maxLeft + minRight) / 2.0;
+                }
+            }
+            return 0;
+    }
+
+    static void  exchange(int m,int n){
+        System.out.println(m+" "+n);
+        m = m+n;
+        n = m - n;
+        m = m-n;
+        System.out.println(m+" "+n);
+    }
+
+    public static void main(String[] args) {
+        exchange(1,2);
+    }
 }
